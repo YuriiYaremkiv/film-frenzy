8780c1d270fbb9966432bb6a9aa57e3bbb2096c7
Yuriy Yaremkiv <y.yaremkiv@gmail.com>
2022-11-06 21:26:28 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import { Layout } from './Layout/Layout';
 import { HomePage } from '../pages/HomePage/HomePage';
 import { MovieDetails } from '../pages/MovieDetails/MovieDetails';
-import { MoviesPage } from '../pages/MoviesPage/MoviesPage';
+import { Movies } from '../pages/Movies/Movies';
 import { Cast } from './Cast/Cast';
 import { Reviews } from './Reviews/Reviews';
 
@@ -12,7 +12,7 @@ export const App = () => {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path="movies" element={<MoviesPage />} />
+        <Route path="movies" element={<Movies />} />
         <Route path="movies/:movieId" element={<MovieDetails />}>
           <Route path="cast" element={<Cast />} />
           <Route path="reviews" element={<Reviews />} />
