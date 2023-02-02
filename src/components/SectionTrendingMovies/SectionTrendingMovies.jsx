734d9d4bce584dd95d57438fb8fcb8b32f54feff
yyaremkiv <y.yaremkiv@gmail.com>
2023-02-02 15:55:06 +0200

    add_button_trending

--- src/components/SectionTrendingMovies/SectionTrendingMovies.jsx
+++ src/components/SectionTrendingMovies/SectionTrendingMovies.jsx
@@ -1,14 +1,68 @@
 import css from './SectionTrendingMovies.module.scss';
 
-import React from 'react';
-import { MoviesList } from 'components/MoviesList/MoviesList';
+import { useState } from 'react';
 import { SliderMovies } from 'components/SliderMovies/SliderMovies';
 
 export const SectionTrendingMovies = ({ movies }) => {
+  const [time, setTime] = useState('today');
+
+  const handleChangeRadioButton = e => {
+    setTime(e.target.value);
+  };
+
   return (
     <section>
       <div className="container">
         <h2 className={css.title}>Trending</h2>
+        <div className={css.selector}>
+          <label
+            className={
+              time === 'today' ? css.select__active : css.select__disable
+            }
+          >
+            <h3
+              className={
+                time === 'today'
+                  ? css.select__activeTitle
+                  : css.select__disableTitle
+              }
+            >
+              Today
+            </h3>
+            <input
+              type="radio"
+              checked={time === 'today'}
+              name="time"
+              value="today"
+              onChange={handleChangeRadioButton}
+              className={css.select__input}
+            />
+          </label>
+          <label
+            className={
+              time === 'this-week' ? css.select__active : css.select__disable
+            }
+          >
+            <h3
+              className={
+                time === 'this-week'
+                  ? css.select__activeTitle
+                  : css.select__disableTitle
+              }
+            >
+              This Week
+            </h3>
+            <input
+              type="radio"
+              checked={time === 'this-week'}
+              name="time"
+              value="this-week"
+              onChange={handleChangeRadioButton}
+              className={css.select__input}
+            />
+          </label>
+          <div className={css.backGroung}></div>
+        </div>
         <SliderMovies movies={movies} />
       </div>
     </section>
--- src/components/SliderMovies/SliderMovies.jsx
+++ src/components/SliderMovies/SliderMovies.jsx
@@ -3,43 +3,68 @@ import Slider from 'react-slick';
 
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import Media from 'react-media';
 
 import css from './SliderMovies.module.scss';
 
-import image from '../../images/images.jpg';
-
 export class SliderMovies extends Component {
   render() {
-    const settings = {
-      className: 'center',
-      infinite: true,
-      centerPadding: '60px',
-      slidesToShow: 5,
-      swipeToSlide: true,
-      afterChange: function (index) {
-        console.log(
-          `Slider Changed to: ${index + 1}, background: #222; color: #bada55`
-        );
-      },
-    };
-
     const { movies } = this.props;
 
     return (
-      <div>
-        <Slider {...settings}>
-          {movies.map(({ title, name, poster_path: path }) => (
-            <div>
-              <img
-                src={`https://image.tmdb.org/t/p/w500/${path}`}
-                alt="ias"
-                width="250"
-              />
-              <p>{title || name}</p>
-            </div>
-          ))}
-        </Slider>
-      </div>
+      <Media
+        queries={{
+          responsive: '(max-width: 479px)',
+          mobile: '(max-width: 767px)',
+          tablet: '(min-width: 768px) and (max-width: 1199px)',
+          desktop: '(min-width: 1200px)',
+        }}
+      >
+        {matches => (
+          <Slider
+            className="center"
+            infinite={true}
+            centerPadding="60px"
+            swipeToSlide={true}
+            slidesToShow={
+              matches.responsive
+                ? 1
+                : matches.mobile
+                ? 2
+                : matches.tablet
+                ? 6
+                : 8
+            }
+          >
+            {movies.map(({ id, title, name, poster_path: path }) => (
+              <div key={id} className={css.card}>
+                <img
+                  src={`https://image.tmdb.org/t/p/w500/${path}`}
+                  alt="ias"
+                  className={css.image}
+                />
+                <p className={css.title}>{title || name}</p>
+                <p className={css.title}>{title || name}</p>
+              </div>
+            ))}
+          </Slider>
+        )}
+      </Media>
     );
   }
 }
+
+// <div>
+//   <Slider {...settings}>
+//     {movies.map(({ title, name, poster_path: path }) => (
+//       <div className={css.card}>
+//         <img
+//           src={`https://image.tmdb.org/t/p/w500/${path}`}
+//           alt="ias"
+//           width="250"
+//         />
+//         <p>{title || name}</p>
+//       </div>
+//     ))}
+//   </Slider>
+// </div>;
