87a3ecddc10f33ce271d819d001b008a2a5a571d
yyaremkiv <y.yaremkiv@gmail.com>
2023-03-09 21:11:08 +0200

    update

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { Provider } from 'react-redux';
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <Provider store={store}>
-    <BrowserRouter basename="/filmfrenzy">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </Provider>
